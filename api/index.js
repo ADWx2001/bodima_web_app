2be0a19cdafebe44ab441ec0893442b63d9293f8
asanka119 <asankadilshanme@gmail.com>
2024-03-12 01:45:36 +0530

    successfully connected with mongodb

--- api/index.js
+++ api/index.js
@@ -1,14 +1,24 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
+dotenv.config();
 
+//connect with mongodb
+mongoose.connect(process.env.MONGO_URL).then(()=>{
+    console.log('Connected to mongodb');
+}).catch((err)=>{
+    console.log(err);
+})
 
 const app = express();
-app.use(express.json());
+app.use(express.json()); //default middleware
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res)=>{ //test case
     res.send("Hi");
 })
 
+// create local server
 const port = 3000;
 app.listen(port, ()=>{
     console.log(`Server is running on Port: ${port}`);
