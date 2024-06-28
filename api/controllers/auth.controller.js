de674ac3dd67f03b4b0a96658eeebad061e468c5
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-06-27 23:29:31 -0700

    create sign up api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,18 @@
 import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
 
 export const auth = async (req, res)=>{
     const { username, email, password} = req.body;
-
-    const newUser = new User({ username, email, password});
-    await newUser.save();
-    res.status(201).json('User created successfully!!');
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword});
+    
+    try {
+        await newUser.save();
+        res.status(201).json('User created successfully!!');
+        
+    } catch (error) {
+        res.status(500).json(error.message);
+    }
 
 
 };
