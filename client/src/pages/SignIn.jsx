d0ac270e55dbfe62c031e25cbdaa1a559cb277a1
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-06-29 00:31:59 -0700

    "Added Redux store and provider to main.jsx, and implemented Redux state management in SignIn.jsx"

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,14 +1,16 @@
 import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 
 export default function SignIn() {
   const [formData, setFormData] = useState({
     email: "",
     password: "",
   });
-  const [errors, setErrors] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleChange = (e) => {
     setFormData({
@@ -21,7 +23,8 @@ export default function SignIn() {
     e.preventDefault();
 
     try {
-      setLoading(true); // make loading true before request
+      //setLoading(true); // make loading true before request
+      dispatch(signInStart());
       const response = await fetch("/api/auth/sign-in", {
         method: "POST",
         headers: {
@@ -33,36 +36,33 @@ export default function SignIn() {
       const data = await response.json();
 
       if (data.success === false) {
-        setErrors(data.message);
-        setLoading(false);
+        dispatch(signInFailure(error.message));
         return;
       }
-      setLoading(false);
-      setErrors(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setErrors(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
 
   return (
     <div className="p-3 max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <h1 className="text-3xl text-center font-semibold my-7"><span className="text-orange-500">Sign</span> In</h1>
       <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         
         <input
           type="email"
           name="email"
           onChange={handleChange}
-          className="p-3 rounded-lg border"
+          className="p-3 rounded-lg border focus:outline-orange-500"
           placeholder="Email"
         />
         <input
           type="password"
           name="password"
           onChange={handleChange}
-          className="p-3 rounded-lg border"
+          className="p-3 rounded-lg border focus:outline-orange-500"
           placeholder="Password"
         />
         <button
@@ -88,7 +88,7 @@ export default function SignIn() {
           <p className="text-blue-600">Sign Up</p>
         </Link>
       </div>
-      {errors && <p className="text-red-500">{errors}</p>}
+      {error && <p className="text-red-500">{error}</p>}
     </div>
   );
 }
