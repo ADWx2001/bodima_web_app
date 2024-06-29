11b5e6e957f21f6b3ba5da5a4bc6f0f310e0e2c3
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-06-28 23:54:21 -0700

    "Added signin functionality to auth controller and route, and implemented signin page in client"

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,8 +1,8 @@
 import express from 'express';
-import { auth } from '../controllers/auth.controller.js';
+import { auth, signin } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
 router.post("/sign-up", auth);
-
+router.post("/sign-in", signin);
 export default router;
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,7 +1,94 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [errors, setErrors] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      setLoading(true); // make loading true before request
+      const response = await fetch("/api/auth/sign-in", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await response.json();
+
+      if (data.success === false) {
+        setErrors(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setErrors(null);
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      setErrors(error.message);
+    }
+  };
+
   return (
-    <div>Signin</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        
+        <input
+          type="email"
+          name="email"
+          onChange={handleChange}
+          className="p-3 rounded-lg border"
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          onChange={handleChange}
+          className="p-3 rounded-lg border"
+          placeholder="Password"
+        />
+        <button
+          disabled={loading}
+          type="submit"
+          className="bg-slate-700 hover:opacity-95 disabled:opacity-80 text-white p-3 rounded-lg"
+        >
+          {loading ? "Loading..." : "Sign In"}
+        </button>
+        <button
+          type="button"
+          className="bg-rose-700 hover:opacity-95 disabled:opacity-80 text-white p-3 rounded-lg"
+          onClick={() => {
+            // Implement your Google sign-in logic here
+          }}
+        >
+          Continue with Google
+        </button>
+      </form>
+      <div className="flex gap-2 mt-3">
+        <p className="">Dont have an account?</p>
+        <Link to="/sign-up">
+          <p className="text-blue-600">Sign Up</p>
+        </Link>
+      </div>
+      {errors && <p className="text-red-500">{errors}</p>}
+    </div>
+  );
 }
