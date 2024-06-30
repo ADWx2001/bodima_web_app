44519de6d2145bd1086376e52a02010c86f5094e
Asanka Wickramasurendra <asankadilshanme@gmail.com>
2024-06-29 23:46:46 -0700

    "Added PrivateRoute component to protect Profile route and updated Header component to display user avatar if logged in"

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,9 @@
 import {FaSearch} from 'react-icons/fa'
 import { Link } from 'react-router-dom'
-
+import { useSelector } from 'react-redux'
 export default function Header() {
+    const {currentUser} = useSelector(state => state.user);
+
   return (
     
     <header className='bg-slate-200 shadow-md'>
@@ -27,8 +29,12 @@ export default function Header() {
                     <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
                 </Link>
 
-                <Link to='/sign-in'>
+                <Link to='/profile'>
+                {currentUser ? 
+                    <img src={currentUser.avatar} alt='profile' className='w-7 h-7 rounded-full'/>
+                :
                     <li className='hidden sm:inline text-slate-700 hover:underline'>Sign In</li>
+                }
                 </Link>
             </ul>
         </div>
